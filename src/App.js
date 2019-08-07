eaa779639a9e0dca6c1551c86dc2e4dd2fe50068
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-08-07 20:44:56 +0100

    WIP: setup form fields and withFormik higher order component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import Form from './Form'
+import NewUserForm from './Form'
 
 function App() {
   return (
     <div className="App">
       <p>placeholder for apps component</p>
-      <Form />
+      <NewUserForm />
     </div>
   );
 }
--- src/Form.js
+++ src/Form.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import { Form, Field, withFormik } from 'formik'
 
-const Form = () => {
- return (
-  <p>placeholder for form component</p>
- ) 
+const NewUserForm = ({ errors, touched, values }) => {
+  return (
+    <Form>
+      <Field type='text' name='name' placeholder='Name'/>
+      <Field type='email' name='email' placeholder='Email'/>
+      <Field type='password' name='password' placeholder='Password'/>
+      {/* <Field type='checkbox' name='tos' placeholder='Name'/> */}
+      <button>Submit</button>
+    </Form>
+  )
 }
 
-export default Form
+const FormikNewUserForm = withFormik({
+  mapPropsToValues({ name, email, password }) {
+    return {
+      name: name || '',
+      email: email || '',
+      password: password || ''
+    }
+  }
+})(NewUserForm)
+
+export default FormikNewUserForm
